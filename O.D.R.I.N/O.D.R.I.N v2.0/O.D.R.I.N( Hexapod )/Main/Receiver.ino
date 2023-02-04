ba9b426da5c8823fa33c78b3ce7eb51a641e1c57
Nathan Busse <82398683+Nathan-Busse@users.noreply.github.com>
2023-02-04 18:50:24 +0200

    Create Receiver.ino

---
+++ O.D.R.I.N/O.D.R.I.N v2.0/O.D.R.I.N( Hexapod )/Main/Receiver.ino
@@ -0,0 +1,35 @@
+#include <SPI.h>
+#include<RF24.h>
+RF24 radio(7, 8); // CE, CSN
+const byte address[6] = “00001”;
+#define led1 3
+#define led2 5
+#define led3 6
+#define led4 9
+int val[4];
+
+void setup() {
+pinMode(led1, OUTPUT);
+pinMode(led2, OUTPUT);
+pinMode(led3, OUTPUT);
+pinMode(led4, OUTPUT);
+radio.begin();
+radio.openReadingPipe(0, address);
+radio.setDataRate(RF24_2MBPS);
+radio.setPALevel(RF24_PA_HIGH);
+radio.setChannel(0x4C);
+radio.startListening();
+}
+
+void loop() {
+
+delay(5);
+radio.startListening();
+while (!radio.available());
+radio.read(&val, sizeof(val));
+analogWrite(led1, val[0]);
+analogWrite(led2, val[1]);
+analogWrite(led3, val[2]);
+analogWrite(led4, val[3]);
+
+}
